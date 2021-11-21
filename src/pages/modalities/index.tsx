c81b7dc5ee46d58174cb79472db515fec598bbff
Lucas <araujolucas3005@gmail.com>
2021-11-21 19:41:13 -0300

    fixed next Link error

--- src/pages/modalities/index.tsx
+++ src/pages/modalities/index.tsx
@@ -12,53 +12,52 @@ export default function Modalities() {
   const { modalities, removeModality } = useModalities();
 
   return (
-      <div className="flexRow">
-        <SidebarMenu />
-        <div className={styles.content}>
-          <div className={styles.tableWrapper}>
-            <div className={styles.buttonWrapper}>
-              <Link href="/modalities/create" passHref>
+    <div className="flexRow">
+      <SidebarMenu />
+      <div className={styles.content}>
+        <div className={styles.tableWrapper}>
+          <div className={styles.buttonWrapper}>
+            <Link href="/modalities/create" passHref>
+              <a>
                 <ButtonPrimary>+</ButtonPrimary>
-              </Link>
-            </div>
-            {modalities.length === 0 ? (
-              <p>Nenhuma modalidade cadastrada</p>
-            ) : (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>Descrição</th>
-                    <th />
-                  </tr>
-                </thead>
-                <tbody>
-                  {modalities.map((modality) => (
-                    <tr key={modality.id}>
-                      <td>{modality.name}</td>
-                      <td>{modality.description}</td>
-                      <td>
-                        <HStack
-                          spacing="1"
-                          style={{ justifyContent: "center" }}
-                        >
-                          <AiOutlineInfoCircle size="20" />
-                          <AiFillEdit size="20" color="gray" />
-                          <AiFillDelete
-                            style={{ cursor: "pointer" }}
-                            size="20"
-                            color="red"
-                            onClick={() => removeModality(modality.id)}
-                          />
-                        </HStack>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
+              </a>
+            </Link>
           </div>
+          {modalities.length === 0 ? (
+            <p>Nenhuma modalidade cadastrada</p>
+          ) : (
+            <table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Descrição</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {modalities.map((modality) => (
+                  <tr key={modality.id}>
+                    <td>{modality.name}</td>
+                    <td>{modality.description}</td>
+                    <td>
+                      <HStack spacing="1" style={{ justifyContent: "center" }}>
+                        <AiOutlineInfoCircle size="20" />
+                        <AiFillEdit size="20" color="gray" />
+                        <AiFillDelete
+                          style={{ cursor: "pointer" }}
+                          size="20"
+                          color="red"
+                          onClick={() => removeModality(modality.id)}
+                        />
+                      </HStack>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
+    </div>
   );
 }
